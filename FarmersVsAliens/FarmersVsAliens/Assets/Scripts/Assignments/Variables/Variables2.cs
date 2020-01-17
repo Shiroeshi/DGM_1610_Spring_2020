f50ce04ba48bd70a715a4846ad91e9e110fde793
Jonathan Alvarez <jonathanalvarez4@gmail.com>
2020-01-16 22:58:57 -0700

    Tested variables using a return command with an exercise theme

---
+++ FarmersVsAliens/FarmersVsAliens/Assets/Scripts/Assignments/Variables/Variables2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables2 : MonoBehaviour
+{
+    public float Breakfast;
+    public float Lunch;
+    public float Dinner;
+    public float Snacks;
+    public float Calories;
+    public float Exercise;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void Meals(float Breakfast, float Lunch, float Dinner, float Snacks, float Exercise)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Calories;
+        Calories = Breakfast + Lunch + Dinner + Snacks - Exercise;
+
+        Console.WriteLine("I need to exercise more because I eat this many calories a day", Calories);
+    }
+}
