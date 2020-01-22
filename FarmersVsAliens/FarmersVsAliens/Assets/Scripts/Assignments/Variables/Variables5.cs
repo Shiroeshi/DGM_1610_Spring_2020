c6c91b88305212274adc207400da7b2882777e9d
Jonathan Alvarez <jonathanalvarez4@gmail.com>
2020-01-21 23:30:53 -0700

    Experimented using a bool command and how to use True/False statements

---
+++ FarmersVsAliens/FarmersVsAliens/Assets/Scripts/Assignments/Variables/Variables5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables5 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     float meN = 5;
+     float womeN = 10;
+     float childreN = 20;
+
+     int candY = 20;
+
+        People(meN, womeN, childreN);
+
+        Debug.Log(candY > meN + womeN + childreN);
+    }
+
+    private void People(float meN, float womeN, float childreN)
+    {
+        throw new NotImplementedException();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
