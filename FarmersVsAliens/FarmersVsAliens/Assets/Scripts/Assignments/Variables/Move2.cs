be21bb681596cf07742cefc056214aa4b3809e68
Jonathan Alvarez <jonathanalvarez4@gmail.com>
2020-01-28 20:27:15 -0700

    Make models and tried making a course for ball with collision.

---
+++ FarmersVsAliens/FarmersVsAliens/Assets/Scripts/Assignments/Variables/Move2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Here we go!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    // Detect collision with another object
+    void OnCollisionEnter(Collision other)  
+    {
+
+        if (other.gameObject.CompareTag("Track"))
+        {
+            Debug.Log("Wheeeeeeee!");
+        }
+        else if (other.gameObject.CompareTag("Wall"))
+        {
+            Debug.Log("Ow!");
+        }
+        else  
+        {
+            Debug.Log("I'm Flying!");
+        }
+    }
+
+    void OnTriggerEnter(Collider Other)    
+    {
+        Debug.Log("Finish!");
+    }
+}
