498af790099a158bfcd1c6b9f5e58e87f2407388
Jonathan Alvarez <jonathanalvarez4@gmail.com>
2020-02-01 23:53:51 -0700

    Made a jumping script with small course.

---
+++ FarmersVsAliens/FarmersVsAliens/Assets/Scripts/Assignments/Variables/MoveJ.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveJ : MonoBehaviour
+{
+    public float speed;
+    public float verticalInput;
+    public float horizontalInput;
+    public float turnSpeed;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");  
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput); 
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+
+        if(Input.GetKey("space"))
+        {
+            Debug.Log("You are hitting the space bar");
+            transform.Translate(0, 0.15f, 0 * Time.deltaTime);
+        }
+
+    }
+
+    void OnTriggerEnter(Collider Other)
+    {
+        Debug.Log("Finish!");
+    }
+}
