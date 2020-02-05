b8c70b2c955d44dcf4794f6217e8343f5a0a493e
Jonathan Alvarez <jonathanalvarez4@gmail.com>
2020-02-04 21:51:56 -0700

    Made a following camera script to go with the motion and jumping

---
+++ FarmersVsAliens/FarmersVsAliens/Assets/Scripts/Assignments/Variables/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    GameObject playerChar;
+    Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerChar = GameObject.Find("playerChar");
+        cameraOffset = new Vector3(0, 2, -5);  /* seting the Vector3 variable at the start will offset the position of the camera. 
+                                                  Since it is put in Start and not Update, it will only do this translation once.*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerChar.transform.position + cameraOffset; // If you do not alter the position of the camera, the camera will be at the same positon, giving it a first person kind of view.
+    }
+}
