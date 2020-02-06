f1954f97f1836c025f02e17b571ca32426ed130a
Jonathan Alvarez <jonathanalvarez4@gmail.com>
2020-02-05 17:09:43 -0700

    Re caped on  game pickups

---
+++ FarmersVsAliens/FarmersVsAliens/Assets/Scripts/Assignments/Variables/GamePickup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePickup : MonoBehaviour
+{
+
+    public int pointsToAdd;
+    public string pickupName;
+    public string pickupType;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //check to see if trigger detected player and only player. colliding with any other object will not trigger the change
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ScoreManager.AddPoints (pointsToAdd); //always add points first THEN destroy the object. if the other way around, nothing is going to happen and the object will be gone
+            Destroy(gameObject);
+            
+        }
+    }
+}
---
+++ FarmersVsAliens/FarmersVsAliens/Assets/Scripts/Assignments/Variables/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public static int score;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Game score: " + score);
+    }
+
+    public static void AddPoints(int pointsToAdd) //setting it as public makes it so that other codes can interact with this script.
+    {
+        score += pointsToAdd;
+    }
+}
