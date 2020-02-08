5812b16a7e74ea638239b685b45ea3f9921c8885
Jonathan Alvarez <jonathanalvarez4@gmail.com>
2020-02-07 22:22:15 -0700

    Used Sin function in order to make pickup float

---
+++ FarmersVsAliens/FarmersVsAliens/Assets/Scripts/Assignments/Variables/PickupFloat.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupFloat : MonoBehaviour
+{
+
+    // Set objects to float up and down while spinning
+    public float degreesPerSecond = 15f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    // position storage for movement
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // This will mark and record the starting position and rotation of the pickup for use in later variables
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // This makes it spin on the Y
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+        // This makes it float up and down using a Sin() algorithm
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
+
