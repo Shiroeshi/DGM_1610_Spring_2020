acfbf88a6b372db26ab02bb58bb95fc43662d90a
Jonathan Alvarez <jonathanalvarez4@gmail.com>
2020-03-25 17:27:44 -0600

    learned about enumerators and switches

---
+++ FarmersVsAliens/FarmersVsAliens/Assets/Scripts/Assignments/Homework Scripts/Switch.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public string goodFriends;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* if (goodFriends == "Samantha")
+         {
+             print("Wanna sing something?");
+         }
+         else if (goodFriends == "Arec")
+         {
+             print("Wanna play some crucible?")
+         }
+
+         switch (goodFriends) // if any of the string values equals any of these values, it will print out the selected value. This is something we do manually.
+         {
+             case "Samantha":
+                 print("Wanna sing something?");
+                 break;
+
+             case "Arec":
+                 print("Wanna play soem crucible?");
+                 break;
+
+         }
+         */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (goodFriends) // if any of the string values equals any of these values, it will print out the selected value. This is something we do manually.
+        {
+            case "Samantha":
+                print("Wanna sing something?");
+                break;
+
+            case "Arec":
+                print("Wanna play soem crucible?");
+                break;
+
+        }
+
+        switch (starts)
+        {
+            case 1:
+                print("something singing");
+                break;
+            case 2:
+                print("something crucible");
+                break;
+            default:
+                print("where are all my friends...");
+                break;
+        }
+    }
+}
