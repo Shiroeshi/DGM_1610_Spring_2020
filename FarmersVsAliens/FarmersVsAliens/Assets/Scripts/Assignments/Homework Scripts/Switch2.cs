9d448531991e348930aa4a7709fe53bf161867e2
Jonathan Alvarez <jonathanalvarez4@gmail.com>
2020-04-07 20:33:20 -0600

    extra practice scripts with switches

---
+++ FarmersVsAliens/FarmersVsAliens/Assets/Scripts/Assignments/Homework Scripts/Switch2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch2 : MonoBehaviour
+{
+    public string skateBoard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (skateBoard == "Dinghy")
+           {
+               print("Wanna go for a casual cruise?");
+           }
+           else if (skateBoard == "Longboard")
+           {
+                print("Let's do some dancing!");
+           }
+
+            else if (skateBoard == "Skateboard")
+            {
+            print("Let's practice some tricks");
+            }
+
+           switch (skateBoard) 
+           {
+            case "Dinghy":
+                print("Wanna go for a cruise?");
+                   break;
+
+            case "Skateboard":
+                print("Let's practice some tricks");
+                   break;
+
+            case "Longboard":
+                print("Let's do some dancing!");
+                break;
+
+           }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
