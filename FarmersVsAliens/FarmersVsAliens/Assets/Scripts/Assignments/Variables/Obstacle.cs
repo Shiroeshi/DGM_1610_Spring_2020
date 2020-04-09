50f5f2696bb27f1686fdadec71afc3a0d0190bd0
Jonathan Alvarez <jonathanalvarez4@gmail.com>
2020-04-08 22:23:17 -0600

    Made new scripts for scriptable objects

---
+++ FarmersVsAliens/FarmersVsAliens/Assets/Scripts/Assignments/Variables/Obstacle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This has to come before the public class or else it will not compile correctly. This makes it a new menu item for creating objects.
+[CreateAssetMenu(fileName ="New SO", menuName ="Scriptable Object")]
+public class Obstacle : ScriptableObject
+{
+    // Overall this script is making a premade object with scripts that will interact uniformly. It's like a shortcut for adding a whole bunch of scripts for npc's
+
+
+    public new string name; //This is the area for the name
+
+    public string description;
+
+    public GameObject model;
+
+    public Color color;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FarmersVsAliens/FarmersVsAliens/Assets/Scripts/Assignments/Variables/Test Object.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : MonoBehaviour
+{
+    //Using this script makes it so that anything attached to this game object will have these behaviors 
+
+    public Obstacle data;
+
+    private GameObject model;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        model = data.model;
+
+        var modelRend = model.GetComponent<Renderer>();
+        modelRend.sharedMaterial.SetColor("_Color", data.color);
+
+        print(data.name);
+        print(data.description);
+    }
+}
