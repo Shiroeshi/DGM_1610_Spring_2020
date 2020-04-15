7df96875dc8fa605a6ee38c5107985fecd05cb3f
Jonathan Alvarez <jonathanalvarez4@gmail.com>
2020-04-14 21:18:26 -0600

    edited triple shot scripts

--- FarmersVsAliens/FarmersVsAliens/Assets/Scripts/Assignments/Homework Scripts/TripleShot.cs
+++ FarmersVsAliens/FarmersVsAliens/Assets/Scripts/Assignments/Homework Scripts/TripleShot.cs
@@ -8,33 +8,22 @@ public class TripleShot : MonoBehaviour
     public Rigidbody projectilePrefab;
     public int NumberofBullets = 3;
     private float ProjectileOffset = 1.3f;
+    public Rigidbody player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("mouse 0"))
+        if (Input.GetKeyDown("mouse 0"))
         {
-            StartCoroutine(Shoot());
-        }
-    }
-
-    IEnumerator Shoot()
-    {
-        Vector3 Position = new Vector3(transform.position.x,
-                                      transform.position.y + ProjectileOffset,
-                                      transform.position.z);
-        for (int i = 0; i < NumberofBullets; i++)
-        {
-            Instantiate(projectilePrefab,
-                        Position,
-                        Quaternion.identity);
-            yield return new WaitForSeconds(BulletFireRate);
+            Instantiate(projectilePrefab, player.transform.position, projectilePrefab.transform.rotation);
+            projectilePrefab.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 44444) * Time.deltaTime, ForceMode.VelocityChange);
         }
     }
 }
+
